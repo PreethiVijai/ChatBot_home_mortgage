1e88a9fbb0187882cd19b52eca7c088506447fe0
Preethi Vijai Lilly <prvijail@silabs.com>
2021-12-17 16:58:02 -0600

    modified package json for heroku deployment

--- server/main.js
+++ server/main.js
@@ -4,6 +4,6 @@ const calculation = require('./routes/mortgage_calc')
 app.use('/',calculation)
 app.use('/mortgage',calculation)
 app.use('/mortgage/:downPayment/:monthlyPayment',calculation)
-app.listen(8000, function () {
-  console.log('Listening to Port 8000');
+app.listen(process.env.PORT || 8000, function () {
+  console.log('Server started successfully');
 });
