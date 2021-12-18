c4919a9511bfffc48b579b1914773f28cfda57ab
Preethi Vijai Lilly <prvijail@silabs.com>
2021-12-17 18:42:49 -0600

    modified package json for heroku deployment

--- server/main.js
+++ server/main.js
@@ -6,8 +6,7 @@ import router from './routes/mortgage_calc.js'
 app.use('/',router)
 app.use('/mortgage',router)
 app.use('/mortgage/:downPayment/:monthlyPayment',router)
-app.listen(8000, function () {
+app.listen(process.env.PORT || 8000, function () {
   console.log('Server started successfully');
 });
 
-//process.env.PORT || 
